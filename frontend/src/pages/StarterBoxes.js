bc90e701d7841c4bc36bcc9c8382a71bd43ab620
JosephASandoval <josephasandoval17@gmail.com>
2021-08-02 19:56:43 -0700

    added brute force createBoxes function

--- frontend/src/pages/StarterBoxes.js
+++ frontend/src/pages/StarterBoxes.js
@@ -6,7 +6,83 @@ import useColorCount from "../hooks/useColorCount";
 function StarterBoxes() {
   const { numBrushes, numStarter, family } = useContext(Context);
   const { colorCount } = useColorCount();
-  const { blue, green, pink } = colorCount(family);
+  const colorCountObj = colorCount(family);
+  // const { blue, green, pink } = colorCountObj;
+
+  function createBoxes() {
+    const boxesArr = [];
+    let firstColor;
+    let secondColor;
+
+    const colorCountArr = Object.entries(colorCountObj);
+
+    colorCountArr.sort((a, b) => b[1] - a[1]);
+
+    for (let idx = 0; idx < colorCountArr.length; idx++) {
+      let [currentColor, currentValue] = colorCountArr[idx];
+      let nextElement = colorCountArr[idx + 1] || null;
+
+      while (currentValue > 0) {
+        if (currentValue >= 2) {
+          firstColor = currentColor;
+          secondColor = firstColor;
+          currentValue -= 2;
+          createBox(firstColor, secondColor, boxesArr);
+        } else if (currentValue === 1 && nextElement) {
+          firstColor = currentColor;
+          secondColor = nextElement[0];
+          currentValue -= 1;
+          nextElement[1] -= 1;
+          createBox(firstColor, secondColor, boxesArr);
+        } else {
+          firstColor = currentColor;
+          secondColor = null;
+          currentValue -= 1;
+          createBox(firstColor, secondColor, boxesArr);
+        }
+      }
+    }
+
+    return boxesArr;
+  }
+
+  function createBox(firstColor, secondColor, arr) {
+    if (firstColor === secondColor) {
+      arr.push(
+        <div className="box">
+          <i className={`color-${firstColor} ri-drop-line`}></i>
+          <div>
+            <h3>2 brushes</h3>
+            <h3>2 replacement heads</h3>
+          </div>
+        </div>
+      );
+    } else if (secondColor && firstColor !== secondColor) {
+      arr.push(
+        <div className="box">
+          <i className={`color-${firstColor} ri-drop-line`}></i>
+          <div>
+            <h3>1 brush</h3>
+            <h3>1 replacement head</h3>
+          </div>
+          <div className={`color-${firstColor}`}>
+            <h3>1 brush</h3>
+            <h3>1 replacement head</h3>
+          </div>
+        </div>
+      );
+    } else {
+      arr.push(
+        <div className="box">
+          <i className={`color-${firstColor} ri-drop-line`}></i>
+          <div>
+            <h3>1 brush</h3>
+            <h3>1 replacement head</h3>
+          </div>
+        </div>
+      );
+    }
+  }
 
   return (
     <div>
@@ -19,14 +95,7 @@ function StarterBoxes() {
       </div>
 
       <h1>Boxes:</h1>
-      <div className="boxes">
-        <h3>{blue} blue brushes</h3>
-        <h3>{blue} blue replacement heads</h3>
-        <h3>{green} green brushes</h3>
-        <h3>{green} green replacement heads</h3>
-        <h3>{pink} pink brush</h3>
-        <h3>{pink} pink replacement head</h3>
-      </div>
+      <div className="boxes">{createBoxes()}</div>
     </div>
   );
 }
