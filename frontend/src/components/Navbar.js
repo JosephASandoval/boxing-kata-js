272e8a4ddc3a5c30d4fce66ea98dbb4da2bd087b
JosephASandoval <josephasandoval17@gmail.com>
2021-08-03 22:31:14 -0700

    Appropriately enable/disable Starter and Refill Box buttons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [isStarterDisabled, setIsStarterDisabled] = useState(true);
+  const [isRefillDisabled, setIsRefillDisabled] = useState(false);
+
+  function disableStarterLink() {
+    setIsStarterDisabled(true);
+    setIsRefillDisabled(false);
+  }
+
+  function disableRefillLink() {
+    setIsRefillDisabled(true);
+    setIsStarterDisabled(false);
+  }
+
+  const styleStarterLink = isStarterDisabled ? "link-disabled" : "";
+  const styleRefillLink = isRefillDisabled ? "link-disabled" : "";
+
   return (
     <nav>
-      <Link to="/">
-        <h2 className="link">Starter Boxes</h2>
+      <Link className={styleStarterLink} to="/">
+        <h2
+          className={isStarterDisabled ? "link-disabled" : "link"}
+          onClick={disableStarterLink}
+        >
+          Starter Boxes
+        </h2>
       </Link>
 
-      <Link to="/refill">
-        <h2 className="link">Refill Boxes</h2>
+      <Link className={styleRefillLink} to="/refill">
+        <h2
+          className={isRefillDisabled ? "link-disabled" : "link"}
+          onClick={disableRefillLink}
+        >
+          Refill Boxes
+        </h2>
       </Link>
     </nav>
   );
